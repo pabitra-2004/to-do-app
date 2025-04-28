03f3e987317678bc0aaa2b7c9ddea82e09290a3f
Souvik Das <souvikdas.dev@gmail.com>
2025-04-28 13:08:15 +0530

    wip: add completed-todo-list feature

--- src/main.js
+++ src/main.js
@@ -36,8 +36,8 @@ function renderTodolist() {
   todos.forEach(function (todo, index, array) {
     todo_list += `<li data-index=${index} class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-2 px-3.5 rounded shadow group">
                     <input type="checkbox" id="todo-checkbox-${index}" class="todo-checkbox peer flex-none" ${
-                      todo.is_done ? "checked" : ""
-                    } />
+      todo.is_done ? "checked" : ""
+    } />
                     <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
                       ${todo.text}
                     </label>
@@ -142,3 +142,34 @@ function toggleSubmitBtns(edit = false) {
     editTodoBtn.classList.replace("inline-flex", "hidden");
   }
 }
+
+document
+  .getElementById("completed-nav")
+  .addEventListener("click", function (event) {
+    const completed_todos = todos.filter((todo) => todo.is_done);
+
+    // console.log(completed_todos);
+
+    let todo_list = "";
+    completed_todos.forEach(function (todo, index, array) {
+      todo_list += `<li data-index=${index} class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-2 px-3.5 rounded shadow group">
+                    <input type="checkbox" id="todo-checkbox-${index}" class="todo-checkbox peer flex-none" ${
+                      todo.is_done ? "checked" : ""
+                    } />
+                    <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
+                      ${todo.text}
+                    </label>
+
+                    <!-- actions -->
+                    <div class="flex-none hidden group-hover:flex items-center justify-center gap-2">
+                      <!-- delete button -->
+                      <button type="button" class="todo-delete-btn hover:bg-red-500 hover:text-gray-100 hover:shadow-lg rounded-full text-sm p-1.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
+                          <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                        </svg>
+                      </button>
+                    </div>
+                </li>`;
+    });
+    todoList.innerHTML = todo_list;
+  });
