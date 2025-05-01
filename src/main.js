75df5ce61042b7a7509bc2e7728de934a696dc54
Souvik Das <souvikdas.dev@gmail.com>
2025-05-01 10:31:42 +0530

    add "mark-important" feature

--- src/main.js
+++ src/main.js
@@ -2,22 +2,27 @@ const todos = [
   {
     text: "And I'd like to take a minute just sit right there",
     is_done: true,
+    is_important: false,
   },
   {
     text: "I'll tell you how I became the prince of a town called Bel-Air",
     is_done: false,
+    is_important: false,
   },
   {
     text: "Now this is a story all about how, my life got flipped-turned upside down",
     is_done: false,
+    is_important: false,
   },
   {
     text: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia",
     is_done: true,
+    is_important: false,
   },
   {
     text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
     is_done: false,
+    is_important: true,
   },
 ];
 
@@ -44,6 +49,20 @@ function renderTodolist() {
 
                     <!-- actions -->
                     <div class="flex-none hidden group-hover:flex items-center justify-center gap-2">
+                      <!-- edit button -->
+                      <button type="button" class="todo-important-btn text-sm p-1.5">
+                        ${
+                          todo.is_important
+                            ? `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
+                          <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>`
+                            : `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                        </svg>`
+                        }
+
+                      </button>
+
                       <!-- edit button -->
                       <button type="button" class="todo-edit-btn hover:bg-yellow-500 hover:text-gray-100 hover:shadow-lg rounded-full text-sm p-1.5">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
@@ -131,6 +150,17 @@ todoList.addEventListener("click", function (event) {
 
     renderTodolist();
   }
+
+  // mark important
+  if (event.target.matches(`button.todo-important-btn[type="button"]`)) {
+    const index = event.target.closest("li").dataset.index;
+
+    todos[index].is_important = !todos[index].is_important;
+
+    console.log(todos);
+
+    renderTodolist();
+  }
 });
 
 function toggleSubmitBtns(edit = false) {
@@ -153,23 +183,23 @@ document
     let todo_list = "";
     completed_todos.forEach(function (todo, index, array) {
       todo_list += `<li data-index=${index} class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-2 px-3.5 rounded shadow group">
-                    <input type="checkbox" id="todo-checkbox-${index}" class="todo-checkbox peer flex-none" ${
-                      todo.is_done ? "checked" : ""
-                    } />
-                    <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
-                      ${todo.text}
-                    </label>
+                      <input type="checkbox" id="todo-checkbox-${index}" class="todo-checkbox peer flex-none" ${
+        todo.is_done ? "checked" : ""
+      } />
+                      <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
+                        ${todo.text}
+                      </label>
 
-                    <!-- actions -->
-                    <div class="flex-none hidden group-hover:flex items-center justify-center gap-2">
-                      <!-- delete button -->
-                      <button type="button" class="todo-delete-btn hover:bg-red-500 hover:text-gray-100 hover:shadow-lg rounded-full text-sm p-1.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
-                          <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
-                        </svg>
-                      </button>
-                    </div>
-                </li>`;
+                      <!-- actions -->
+                      <div class="flex-none hidden group-hover:flex items-center justify-center gap-2">
+                        <!-- delete button -->
+                        <button type="button" class="todo-delete-btn hover:bg-red-500 hover:text-gray-100 hover:shadow-lg rounded-full text-sm p-1.5">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
+                            <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                          </svg>
+                        </button>
+                      </div>
+                    </li>`;
     });
     todoList.innerHTML = todo_list;
   });
