82d3cd6e2d0f9c47d8adcccbe838ee5d1798019b
Souvik Das <souvikdas.dev@gmail.com>
2025-05-01 13:00:47 +0530

    generate unique id for all todos

--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
 const todos = [
   {
+    id: generateId(),
     text: "And I'd like to take a minute just sit right there",
     is_done: true,
     is_important: false,
     due_date: null,
   },
   {
+    id: generateId(),
     text: "I'll tell you how I became the prince of a town called Bel-Air",
     is_done: false,
     is_important: false,
     due_date: null,
   },
   {
+    id: generateId(),
     text: "Now this is a story all about how, my life got flipped-turned upside down",
     is_done: false,
     is_important: false,
     due_date: new Date(),
   },
   {
+    id: generateId(),
     text: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia",
     is_done: true,
     is_important: false,
     due_date: null,
   },
   {
+    id: generateId(),
     text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
     is_done: false,
     is_important: true,
@@ -35,23 +40,46 @@ const todoList = document.getElementById("todo-list");
 const addTodoForm = document.getElementById("addTodoForm");
 const todoTextInput = document.getElementById("todo-text-input");
 const todoDueDateInput = document.getElementById("todo-due-date-input");
-const todoIndexInput = document.getElementById("todo-index-input");
+const todoIdInput = document.getElementById("todo-id-input");
 const addTodoBtn = document.getElementById("add-todo-btn");
 const editTodoBtn = document.getElementById("edit-todo-btn");
 
 // render todos {read}
-function renderTodolist() {
+function renderTodolist(filter = "all") {
   console.log(todos);
 
+  const filtered_todos = todos.filter((todo) => {
+    if (filter == "all") {
+      return true;
+    } else if (filter == "today") {
+      const current_date = getDate(new Date());
+      const todo_due_date = getDate(todo.due_date);
+
+      return current_date === todo_due_date;
+    } else if (filter == "planned") {
+      return todo.due_date;
+    } else if (filter == "important") {
+      return todo.is_important;
+    } else if (filter == "completed") {
+      return todo.is_done;
+    }
+  });
+
+  console.log("filtered_todos", filtered_todos);
+
   let todo_list = "";
-  todos.forEach(function (todo, index, array) {
-    todo_list += `<li data-index=${index} class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-3 px-3.5 rounded shadow group">
+  filtered_todos.forEach(function (todo, index, array) {
+    todo_list += `<li data-id=${
+      todo.id
+    } class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-3 px-3.5 rounded shadow group">
                     <input type="checkbox" id="todo-checkbox-${index}" ${
       todo.is_done ? "checked" : ""
-    } class="todo-checkbox peer flex-none" />
+    } class="todo-checkbox flex-none" />
                     <div class="w-full">
                       <div class="mb-2">
-                        <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
+                        <label for="todo-checkbox-${index}" class="${
+      todo.is_done ? "line-through text-gray-600" : ""
+    } grow select-none">
                           ${todo.text}
                         </label>
                       </div>
@@ -104,22 +132,24 @@ function renderTodolist() {
 addTodoForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const todo_index = todoIndexInput.value;
+  const todo_id = todoIdInput.value;
   const todo_text = todoTextInput.value;
   const todo_due_date = todoDueDateInput.value;
   const due_date = new Date(todo_due_date);
 
   console.log(todo_due_date);
 
-  if (todo_index) {
+  if (todo_id) {
+    const todo = todos.find((todo) => todo.id === todo_id);
     // edit
-    todos[todo_index].text = todo_text;
+    todo.text = todo_text;
     if (due_date) {
-      todos[todo_index].due_date = due_date;
+      todo.due_date = due_date;
     }
   } else {
     // create
     todos.push({
+      id: generateId(),
       text: todo_text,
       is_done: false,
       due_date: due_date,
@@ -129,13 +159,14 @@ addTodoForm.addEventListener("submit", function (event) {
   renderTodolist();
 
   // form goes on create state
-  todoIndexInput.value = "";
+  todoIdInput.value = "";
   todoTextInput.value = "";
   todoDueDateInput.value = "";
 
   toggleSubmitBtns(false);
 });
 
+// initial render
 renderTodolist();
 
 todoList.addEventListener("click", function (event) {
@@ -146,48 +177,46 @@ todoList.addEventListener("click", function (event) {
 
   // select only input[checkbox] with "todo-li" class
   if (event.target.matches(`input.todo-checkbox[type="checkbox"]`)) {
-    const index = event.target.closest("li").dataset.index;
+    const todo_id = event.target.closest("li").dataset.id;
+    const todo = todos.find((todo) => todo.id === todo_id);
 
-    todos[index].is_done = event.target.checked;
+    todo.is_done = event.target.checked;
 
     renderTodolist();
   }
 
   // edit
   if (event.target.matches(`button.todo-edit-btn[type="button"]`)) {
-    const index = event.target.closest("li").dataset.index;
-
-    const todo = todos[index];
+    const todo_id = event.target.closest("li").dataset.id;
+    const todo = todos.find((todo) => todo.id === todo_id);
 
     todoTextInput.value = todo.text;
-    todoIndexInput.value = index;
+    todoIdInput.value = todo_id;
 
-    const converted_local_due_date = convertDateFormat(todo.due_date);
-    // console.log(converted_local_due_date);
-    todoDueDateInput.value = converted_local_due_date;
+    if (todo.due_date) {
+      const converted_local_due_date = convertDateFormat(todo.due_date);
+      todoDueDateInput.value = converted_local_due_date;
+    }
 
     toggleSubmitBtns(true);
   }
 
   // delete
   if (event.target.matches(`button.todo-delete-btn[type="button"]`)) {
-    const index = event.target.closest("li").dataset.index;
-
-    // console.log(index);
+    const todo_id = event.target.closest("li").dataset.id;
+    const index = todos.findIndex((todo) => todo.id === todo_id);
 
     todos.splice(index, 1);
-    // console.log(todos);
 
     renderTodolist();
   }
 
   // mark important
   if (event.target.matches(`button.todo-important-btn[type="button"]`)) {
-    const index = event.target.closest("li").dataset.index;
+    const todo_id = event.target.closest("li").dataset.id;
+    const todo = todos.find((todo) => todo.id === todo_id);
 
-    todos[index].is_important = !todos[index].is_important;
-
-    console.log(todos);
+    todo.is_important = !todo.is_important;
 
     renderTodolist();
   }
@@ -203,37 +232,6 @@ function toggleSubmitBtns(edit = false) {
   }
 }
 
-document
-  .getElementById("completed-nav")
-  .addEventListener("click", function (event) {
-    const completed_todos = todos.filter((todo) => todo.is_done);
-
-    // console.log(completed_todos);
-
-    let todo_list = "";
-    completed_todos.forEach(function (todo, index, array) {
-      todo_list += `<li data-index=${index} class="bg-gray-50 min-h-[50px] flex items-center py-2 gap-2 px-3.5 rounded shadow group">
-                      <input type="checkbox" id="todo-checkbox-${index}" class="todo-checkbox peer flex-none" ${
-        todo.is_done ? "checked" : ""
-      } />
-                      <label for="todo-checkbox-${index}" class="peer-checked:line-through peer-checked:text-gray-600 grow select-none">
-                        ${todo.text}
-                      </label>
-
-                      <!-- actions -->
-                      <div class="flex-none hidden group-hover:flex items-center justify-center gap-2">
-                        <!-- delete button -->
-                        <button type="button" class="todo-delete-btn hover:bg-red-500 hover:text-gray-100 hover:shadow-lg rounded-full text-sm p-1.5">
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 inline-block select-none pointer-events-none">
-                            <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
-                          </svg>
-                        </button>
-                      </div>
-                    </li>`;
-    });
-    todoList.innerHTML = todo_list;
-  });
-
 function convertDateFormat(dateString) {
   const date = new Date(dateString);
   const year = date.getFullYear();
@@ -243,4 +241,18 @@ function convertDateFormat(dateString) {
   const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+
+function getDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+function generateId() {
+  return (
+    Date.now().toString(36) + "-" + Math.random().toString(36).substring(2, 8)
+  );
 }
