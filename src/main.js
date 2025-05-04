9c7f8303b3388b06672e1f73fdfb7e7e5fa8a592
Souvik Das <souvikdas.dev@gmail.com>
2025-05-04 09:40:11 +0530

    format js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import { convertDateFormat, generateId, getDate } from "./utils";
+
+// initial data
 const todos = [
   {
     id: generateId(),
@@ -166,9 +169,6 @@ addTodoForm.addEventListener("submit", function (event) {
   toggleSubmitBtns(false);
 });
 
-// initial render
-renderTodolist();
-
 todoList.addEventListener("click", function (event) {
   // console.log(event.target);
 
@@ -232,27 +232,5 @@ function toggleSubmitBtns(edit = false) {
   }
 }
 
-function convertDateFormat(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  return `${year}-${month}-${day}T${hours}:${minutes}`;
-}
-
-function getDate(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
-
-function generateId() {
-  return (
-    Date.now().toString(36) + "-" + Math.random().toString(36).substring(2, 8)
-  );
-}
+// initial render
+renderTodolist();
